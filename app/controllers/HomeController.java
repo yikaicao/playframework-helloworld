bc56316eb670a9100602c89b873d59e3d395672c
yikaicao <yikaicao27@gmail.com>
2022-08-10 12:07:54 +0800

    feat: add hello world example that takse a parameter.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -26,4 +26,8 @@ public class HomeController extends Controller {
         return ok(views.html.tutorial.render());
     }
 
+    public Result hello(String name) {
+        return ok(views.html.hello.render(name));
+    }
+
 }
